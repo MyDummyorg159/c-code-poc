3ae1c131eaa1d3d25ea7c6f702dd43b24208fdf4
Sitanshu Goradia <103405695+sitanshu28@users.noreply.github.com>
2025-05-01 11:18:21 +0000

    Update sample.c

--- src/sample.c
+++ src/sample.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 
 int main() {
-    int unused_variable = 42; // Intentional unused variable
-    char *memory_leak = malloc(100); // Intentional memory leak
-
     printf("Hello, World!\n");
     return 0;
 }
